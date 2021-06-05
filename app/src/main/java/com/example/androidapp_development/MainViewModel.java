a40afdd7b92ab139bfe9fece9753fca7977c7fc0
yergu <56776980+yergu@users.noreply.github.com>
2021-06-05 10:44:35 -0700

    assignmen locaction and other error fix

--- app/src/main/java/com/example/androidapp_development/MainViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.androidapp_development;
-
-import androidx.lifecycle.ViewModel;
-
-public class MainViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/androidapp_development/assignment6/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.androidapp_development.assignment6;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {UserSettings.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract SettingsDao settingsDao();
-}
